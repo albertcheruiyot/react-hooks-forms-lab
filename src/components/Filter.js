aa119237f62fb70b69efdb5f0266ad606e6904b7
Albert Cheruiyot <bettalbert99@gmail.com>
2024-05-02 16:28:35 +0300

    First commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onCategoryChange, onSearchChange }) {
+  function handleSearchChange(event) {
+    const { value } = event.target;
+    onSearchChange(value);
+  } 
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={search}
+        onChange={handleSearchChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,48 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
+  const [shoppingItems, setShoppingItems] = useState(items);
+
+  function handleSearchChange(text) {
+    setSearchText(text);
+  }
+
+  function handleItemFormSubmit(newItem) {
+    setShoppingItems([...shoppingItems, newItem]);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  /*const itemsToDisplay = shoppingItems.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  });*/
+  const itemsToDisplay = shoppingItems.filter((item) => {
+    if (selectedCategory !== "All" && item.category !== selectedCategory) {
+      return false;
+    }
+    if (searchText.trim() !== "") {
+      // Case-insensitive full match
+      if (item.name.toLowerCase() === searchText.toLowerCase()) {
+        return true;
+      }
+      // Case-insensitive partial match
+      if (item.name.toLowerCase().includes(searchText.toLowerCase())) {
+        return true;
+      }
+      return false;
+    }
+    return true;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={searchText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -30,3 +57,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
